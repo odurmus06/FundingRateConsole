9582b05a419f5a4f053fab2d8fcdce48d4e2e8da
odurmus06 <64792892+odurmus06@users.noreply.github.com>
2025-04-08 15:22:05 +0300

    init
    init

---
+++ FundingRateConsole/Program.cs
@@ -0,0 +1,209 @@
+﻿using Binance.Net.Clients;
+using CryptoExchange.Net.CommonObjects;
+using CryptoExchange.Net.Sockets;
+using FundingRateConsole;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Telegram.Bot;
+
+class Program
+{
+    private static readonly BinanceSocketClient socketClient = new BinanceSocketClient();
+    private static List<FundingRateRecord> fundingRateRecords = new List<FundingRateRecord>();
+    private static readonly string botToken = "7938765330:AAFC6-bpOiffLaa8iSQwpzl0h3FR_yYT4s4";
+    private static readonly string chatId = "7250151162";
+    private static Dictionary<string, DateTime> nonTargetFundingRates = new();
+    private static Dictionary<string, DateTime> TargetFundingRates = new();
+
+    private static decimal firstDestinition = -0.2m;
+    private static decimal secondDestinition = -0.4m;
+    private static decimal speedTrashold = 1;
+    private static bool isOrderActive = false;
+    private static List<(string Symbol, decimal Change)> topGainers = new();
+    static async Task Main(string[] args)
+    {
+        //_ = SendTelegramMessage("Funding Rate Bot Starting...");
+        Console.WriteLine("Funding Rate Bot Starting...");
+        PowerManagement.PreventSleep();
+        await StartSubscription();
+        Console.ReadKey();
+    }
+    private static decimal CalculateFundingRateSpeed(decimal oldFundingRate, DateTime oldTimestamp, decimal newFundingRate, DateTime newTimestamp)
+    {
+        // Funding rate farkını hesapla
+        decimal rateDifference = Math.Abs(newFundingRate) - Math.Abs(oldFundingRate);
+
+        // Geçen süreyi dakika cinsinden hesapla
+        decimal elapsedMinutes = (decimal)(newTimestamp - oldTimestamp).TotalMinutes;
+
+        // Süre sıfır veya negatifse sıfır dön
+        if (elapsedMinutes <= 0)
+        {
+            return 0m;
+        }
+
+        // Funding rate hızını hesapla
+        decimal rateSpeed = rateDifference / elapsedMinutes;
+
+        return rateSpeed;
+    }
+
+    private static async Task SendTelegramMessage(string message)
+    {
+        var botClient = new TelegramBotClient(botToken);
+        await botClient.SendTextMessageAsync(chatId, message);
+    }
+
+    private static async Task StartSubscription()
+    {
+        await startTicker();
+        await SubscribeToTickerUpdatesAsync().ContinueWith(t =>
+        {
+            _ = SendTelegramMessage("Abonelik Başlatılamadı.");
+        }, TaskContinuationOptions.OnlyOnFaulted);
+    }
+
+    private static async Task SubscribeToTickerUpdatesAsync()
+    {
+        BinanceRestClient client = new BinanceRestClient();
+        var symbols = (await client.UsdFuturesApi.ExchangeData.GetBookPricesAsync())
+            .Data
+            .Select(x => x.Symbol)
+            .ToList();
+
+        int batchSize = 20;
+        var symbolBatches = symbols.Select((symbol, index) => new { symbol, index })
+                                   .GroupBy(x => x.index / batchSize)
+                                   .Select(g => g.Select(x => x.symbol).ToList())
+                                   .ToList();
+
+        foreach (var batch in symbolBatches)
+        {
+            var tickerSubscriptionResult = await socketClient.UsdFuturesApi.ExchangeData.SubscribeToMarkPriceUpdatesAsync(batch, null, async (update) =>
+            {
+                decimal fundingRatePercentage = (decimal)(update.Data.FundingRate ?? 0) * 100;
+                var dateTime = update.Data.EventTime.ToString("yyyy-MM-dd HH:mm:ss");
+                var symbol = update.Data.Symbol;
+                var markPrice = update.Data.MarkPrice;
+                
+
+                var negativeThreshold = GetNegativeThreshold();
+
+                await HandleFundingRateAsync(symbol, fundingRatePercentage, dateTime, rate => fundingRatePercentage <= negativeThreshold, markPrice);
+            });
+
+            if (!tickerSubscriptionResult.Success)
+            {
+                Console.WriteLine("Bağlantı başarısız! Yeniden denemeye hazırlanıyor...");
+                //_ = SendTelegramMessage("Bağlantı başarısız! Yeniden denemeye hazırlanıyor...");
+            }
+            else
+            {
+                tickerSubscriptionResult.Data.ConnectionLost +=  () =>
+                {
+                    Console.WriteLine("Bağlantı kayboldu! Yeniden bağlanılıyor...");
+                    //_ = SendTelegramMessage("Bağlantı kayboldu! Yeniden bağlanılıyor...");
+                };
+
+                tickerSubscriptionResult.Data.ConnectionRestored += (reconnectTime) =>
+                {
+                    Console.WriteLine($"Bağlantı geri geldi: {reconnectTime}");
+                    //_ = SendTelegramMessage($"Bağlantı geri geldi: {reconnectTime}");
+                };
+            }
+        }
+
+        await Task.Delay(-1);
+    }
+    private static async Task startTicker()
+    {
+        var tickerSubscriptionResult = await socketClient.UsdFuturesApi.ExchangeData.SubscribeToAllTickerUpdatesAsync(update =>
+        {
+            var sorted = update.Data
+               .OrderByDescending(t => t.PriceChangePercent) // Büyükten küçüğe sırala
+               .Take(5) // En büyük 5 tanesini al
+               .Select(t => (t.Symbol, t.PriceChangePercent)) // Gerekli bilgiyi al
+               .ToList();
+
+            topGainers = sorted;
+
+        });
+ 
+    }
+    static async Task order()
+    {   
+       await SendTelegramMessage("dd");
+    }
+
+    private static decimal GetNegativeThreshold()
+    {
+        return firstDestinition;
+    }
+
+    private static async Task HandleFundingRateAsync(string symbol, decimal fundingRatePercentage, string dateTime, Func<decimal, bool> condition, decimal price)
+    {
+        try
+        {
+            //Console.WriteLine($"Symbol: {symbol} | Funding Rate: {fundingRatePercentage} | Mark Price: {price}");
+
+
+            if (condition(fundingRatePercentage))
+            {
+                if (nonTargetFundingRates.ContainsKey(symbol))
+                {
+                    TargetFundingRates[symbol] = DateTime.Now;
+                    nonTargetFundingRates.Remove(symbol);
+
+                    Console.WriteLine($"firstDestinition geçildi  - Symbol: {symbol}");
+
+                }
+                if (nonTargetFundingRates.ContainsKey(symbol) && TargetFundingRates.ContainsKey(symbol))
+                {
+                     
+                    nonTargetFundingRates.Remove(symbol);
+
+                }
+
+
+                if (fundingRatePercentage <= secondDestinition && TargetFundingRates.ContainsKey(symbol))
+                {
+                    decimal speed = CalculateFundingRateSpeed(firstDestinition, TargetFundingRates[symbol], fundingRatePercentage, DateTime.Now);
+
+                    Console.WriteLine($"secondDestinition geçildi - Symbol: {symbol}, Hız: {speed.ToString("F4")}");
+
+                    await order();
+
+                }
+            }
+            else
+            {
+                if (!nonTargetFundingRates.ContainsKey(symbol))
+                {
+                    nonTargetFundingRates[symbol] = DateTime.Now;
+                   
+                }
+                else if (TargetFundingRates.ContainsKey(symbol))
+                {
+                    TargetFundingRates.Remove(symbol);
+                }
+               
+
+            }
+        }
+        catch (Exception ex)
+        {
+            Console.WriteLine($"Hata: {ex.Message}");
+        }
+    }
+}
+
+
+public class FundingRateRecord
+{
+    public DateTime Timestamp { get; set; }
+    public decimal FundingRate { get; set; }
+    public decimal Price { get; set; }
+    public required string Symbol { get; set; }
+}
