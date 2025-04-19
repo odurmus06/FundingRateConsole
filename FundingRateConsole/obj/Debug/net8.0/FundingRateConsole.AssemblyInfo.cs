c278fb0a71ea7fc09b814fa4241fb1827c2dd3f0
odurmus06 <64792892+odurmus06@users.noreply.github.com>
2025-04-20 00:04:46 +0300

    sds
    ds

--- FundingRateConsole/obj/Debug/net8.0/FundingRateConsole.AssemblyInfo.cs
+++ FundingRateConsole/obj/Debug/net8.0/FundingRateConsole.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("FundingRateConsole")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a0d324edf979ff10439aadf2f6d629b43ac44dc6")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d735e20ba89104545f8c8b1050ba84112750f5d6")]
 [assembly: System.Reflection.AssemblyProductAttribute("FundingRateConsole")]
 [assembly: System.Reflection.AssemblyTitleAttribute("FundingRateConsole")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
