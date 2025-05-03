fb7a3d520f7d532eb34a99df4aa4f81781e05233
odurmus06 <64792892+odurmus06@users.noreply.github.com>
2025-05-03 14:33:30 +0300

    Update Program.cs

--- FundingRateConsole/Program.cs
+++ FundingRateConsole/Program.cs
@@ -40,8 +40,8 @@ class Program
     private static string apiSecret = "IjP1ZmJXcrRxnep0koHlqnbELxYagXgm295FP0wHG2Ow3QV2jQCasUAyWEmem38l";
     private static string listenKey;
     // Hedef Değerler ve Eşikler
-    private static decimal firstDestinition = -0.4m;
-    private static decimal secondDestinition = -0.45m;
+    private static decimal firstDestinition = -0.39m;
+    private static decimal secondDestinition = -0.40m;
     private static decimal speedTrashold = 1;
 
     // Top Gainers
